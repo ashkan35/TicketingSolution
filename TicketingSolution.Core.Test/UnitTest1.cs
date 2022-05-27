345263f25277489676fa44adf7dd316bad3ebaf4
Ashkan Semsar <ashkansemsar@gmail.com>
2022-05-27 11:44:18 +0430

    Add project files.

---
+++ TicketingSolution.Core.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace TicketingSolution.Core.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
